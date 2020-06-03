108109c623a931a3be1d91430b816f9e5b1f5983
marcelemon <marcelo@lemoncash.io>
2020-06-03 13:35:21 -0300

    fix

--- ledger/truffle-config.js
+++ ledger/truffle-config.js
@@ -10,6 +10,7 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
+  contracts_build_directory: '../server/contracts',
   networks: {
     development: {
       host: "127.0.0.1",
@@ -29,27 +30,27 @@ module.exports = {
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 3,
-      gas: 8000029,
+      gas: 8000000,
       gasPrice: 10000000000,
       timeoutBlocks: 4000
     },
     kovan: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 42,
-      gas: 8000029,
+      gas: 8000000,
       gasPrice: 10000000000
     },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 4,
-      gas: 8000029,
+      gas: 8000000,
       gasPrice: 10000000000
     },
     // main ethereum network(mainnet)
     main: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 1,
-      gas: 8000029,
+      gas: 8000000,
       gasPrice: 10000000000
     }
   }
